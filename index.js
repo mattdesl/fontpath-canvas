48ed9ee45050049365aa4a47e02cbe885311fe4e
mattdesl <dave.des@gmail.com>
2014-08-17 15:09:34 -0400

    breaking the draw call out into its own module

--- index.js
+++ index.js
@@ -1,36 +1,5 @@
 var TextRenderer = require('fontpath-renderer');
-
-var MOVETO = 'm',
-	LINETO = 'l',
-	QUADTO = 'q',
-	CUBICTO = 'c';
-
-/**
- * Draws a glyph as a series of path operations (moveTo, bezierCurveTo, etc),
- * with the optional translation and scaling applied. 
- */
- function decomposeGlyph(context, glyph, scale, x, y) {
-    if (!glyph.path || glyph.path.length===0)
-        return;
-
-    x = x||0;
-    y = y||0;
-    scale = typeof scale === "number" ? scale : 1;
-
-    var path = glyph.path;
-    for (var i=0; i<path.length; i++) {
-        var p = path[i];
-        var f = p[0];
-        if (f===MOVETO) 
-            context.moveTo(p[1]*scale+x, p[2]*-scale+y);
-        else if (f===LINETO) 
-            context.lineTo(p[1]*scale+x, p[2]*-scale+y);
-        else if (f===QUADTO) 
-            context.quadraticCurveTo(p[1]*scale+x, p[2]*-scale+y, p[3]*scale+x, p[4]*-scale+y);
-        else if (f===CUBICTO) 
-            context.bezierCurveTo(p[1]*scale+x, p[2]*-scale+y, p[3]*scale+x, p[4]*-scale+y, p[5]*scale+x, p[6]*-scale+y);
-    }
-};
+var decomposeGlyph = require('fontpath-draw-glyph')
 
 function CanvasRenderer(font, fontSize) {
 	TextRenderer.call(this, font, fontSize);
